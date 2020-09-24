c894537a349c94aefc1e8fd697ef4d770a839136
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-24 06:34:12 +0600

    postgresql added

--- pdfjoiner/settings.py
+++ pdfjoiner/settings.py
@@ -30,8 +30,7 @@ if os.path.exists('secrets.json'):
     secret.close()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 
 # Application definition
 
@@ -81,8 +80,12 @@ WSGI_APPLICATION = 'pdfjoiner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': data["NAME"],
+        'USER': data["USER"],
+        'PASSWORD': data["PASSWORD"],
+        'HOST': data["HOST"],
+        'PORT': data["PORT"]
     }
 }
 
