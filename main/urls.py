0c2633d68bef4b98409864855fb9caa927efc1f9
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-26 04:01:41 +0600

    reset password added

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.user_login, name='login'),
+    path('accounts/login/', views.user_login, name='login'),
     path('logout', views.user_logout, name='logout'),
     path('register', views.user_register, name='register'),
     path('pdfs', views.user_pdf, name='pdfs'),
@@ -16,6 +17,14 @@ urlpatterns = [
     path('profile/edit', views.user_edit, name='edit'),
     path('send', views.send_mail, name='send'),
     path('success', views.PDFHandlerView.as_view()),
+
+    path('reset', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset/sent', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/complete', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
