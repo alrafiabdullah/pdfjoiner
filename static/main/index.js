9b9dcf68d7944e1bde210185d092c66a78917470
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-27 21:59:27 +0600

    minor updates

--- static/main/index.js
+++ static/main/index.js
@@ -62,7 +62,10 @@ document.addEventListener("DOMContentLoaded", () => {
         if (response.message === "File") {
           $(".ajaxProgress").hide();
           $("#submit").show();
-          another.innerHTML = `<h3 class="alert alert-danger">The title is taken. Please add another title!</h3>`;
+          another.innerHTML = `<div  class="alert alert-danger">
+                                <h3>The title is taken. Please add another title!</h3>
+                                <h5>You can try using your_name+course_id+student_id as file name.</h5>
+                                </div>`;
           another.style.display = "block";
         } else if (response.message === "Full") {
           $(".ajaxProgress").hide();
@@ -72,7 +75,12 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
           $(".ajaxProgress").hide();
           $("#pdfForm").hide();
-          another.innerHTML = `<h3 class="alert alert-success">Your PDF is ready!<br>Please click on the button above^</h3>`;
+          const temp = JSON.parse(response.file);
+          const tempID = temp[0].pk;
+          another.innerHTML = `
+            <h3 class="alert alert-success">Your PDF is ready!<br>Please click on the button below.</h3><br>
+            <a target="_blank" href="/views/${tempID}"><button id="download" class="btn btn-primary">Download</button></a>
+          `;
           another.style.display = "block";
         }
       },
