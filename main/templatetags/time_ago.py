9885434786fe5faec4e6e947585987128c2b0a9e
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-27 18:57:39 +0600

    days count function added

--- main/templatetags/time_ago.py
+++ main/templatetags/time_ago.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.filter
 def minutes_ago(time, minutes):
     return time + timezone.timedelta(minutes=minutes) > timezone.now()
+
+
+@register.filter
+def days_ago(time, days):
+    return time + timezone.timedelta(days=days) < timezone.now()
